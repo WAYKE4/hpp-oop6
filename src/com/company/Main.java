f1592e6370bde8e355a259b6c78dc1f38134c8b7
WAYKE4 <gitta.tacrhi@mail.ru>
2023-11-15 22:17:01 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+  //  String word = scanner.nextLine();
+        String word = "dksfjskl DSKLF AKD KQWW АБВСВ АБАБАБАБАБАБАБ AАЯZ";
+        Pattern pattern = Pattern.compile("\\b[A-zА-я]{2,6}\\b ");
+        Matcher matcher = pattern.matcher(word);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+
+    }
+}
